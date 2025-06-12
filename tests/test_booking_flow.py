e7e948f35e07ed1614885558e879700ef0517e9e
jesse26j <jesse26j@gmail.com>
2025-06-12 15:44:16 -0600

    adding PYTHONPATH to ci.yml

--- tests/test_booking_flow.py
+++ tests/test_booking_flow.py
@@ -1,6 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 client = TestClient(app)
 
